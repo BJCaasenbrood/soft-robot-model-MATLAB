fe038cb231342263df9c612b59feaf2fa20a2b37
CirclePipe <b.j.caasenbrood@tue.nl>
2021-08-17 14:43:05 +0200

    removed unnessecary

--- runme.m
+++ runme.m
@@ -1,9 +1,5 @@
+%% RUN ME TO INSTALL
 addpath(genpath('src'));
 addpath(genpath('data'));
 addpath(genpath('examples'));
-clc; clear;
-
-% open mdl_1_link_openloop
-% open mdl_4_link_openloop
-% open mdl_4_link_closedloop
-% open mdl_linear_vs_hyperelastic
+clc; clear; 
